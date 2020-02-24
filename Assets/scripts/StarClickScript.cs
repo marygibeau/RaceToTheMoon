cdff3acb351af60661729686a2c20ffb5aa16d38
Vulfid <54612746+Vulfid@users.noreply.github.com>
2020-02-24 17:36:02 -0500

    Star Click Script
    Star click script added and star sprite. Use {Enter} key to select a star based on the current position of the reticle.

--- Assets/scripts/StarClickScript.cs
+++ Assets/scripts/StarClickScript.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarClickScript : MonoBehaviour
-{
+public class StarClickScript : MonoBehaviour { 
+
+    public GameObject Star = null;
+    public string starName = null;
+    bool canClick = true;
+    public float coolDown = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +17,30 @@ public class StarClickScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         
+        if (Input.GetKey(KeyCode.Return) && canClick) {
+           // Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CircleCollider2D coll = Star.GetComponent<CircleCollider2D>();
+            Vector4 reticlePosition = GameObject.Find("reticle").transform.position;
+
+            if(coll.OverlapPoint(reticlePosition))
+            {
+                print(starName + " clicked");
+                canClick = false;
+                Invoke("CooledDown", coolDown);
+            } else
+            {
+                print("NO STAR CLICKED");
+                canClick = false;
+                Invoke("CooledDown", coolDown);
+            }
+        }
+    }
+
+     void CooledDown()
+    {
+        canClick = true;
     }
 
     private void PrintName(GameObject star)
