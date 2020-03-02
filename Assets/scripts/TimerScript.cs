09f349fda9b6b5b003bb0eac855c6dd51c58fe05
musicknight <music@live.unc.edu>
2020-03-02 15:55:14 -0500

    Added Timer
    TODO placed where function call to initiate the game ending in TimerScript

---
+++ Assets/scripts/TimerScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimerScript : MonoBehaviour
+{
+    Text timerText;
+    float currentTime = 0f;
+    public float startingTime = 5f;
+    Boolean gameOver = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+        timerText = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        //changes color to red when time is below 10 seconds
+        if(currentTime <= 10) {
+            timerText.color = Color.red;
+        }
+
+        if(Math.Ceiling(currentTime) % 60 != 0)
+        {
+        timerText.text = ((int) (currentTime / 60)).ToString() + ":" + (Math.Ceiling(currentTime) % 60).ToString("00");
+        } else if(currentTime > 0){
+            //handling a weird edge case for when the seconds are 00 and the minute decrements a second early
+           timerText.text = ((int) (currentTime / 60) + 1).ToString() + ":" + (Math.Ceiling(currentTime) % 60).ToString("00"); 
+        } else {
+            timerText.text = "0:00";
+            //if the game isn't over and the timer is at 0, end the game
+            if(!gameOver) {
+                //TODO: function call to switch scenes 
+            }
+        }
+        //ensuring the timer doesn't go into the negatives
+        if(currentTime <= 0) 
+        {
+            currentTime = 0;
+        }
+        
+    }
+}
