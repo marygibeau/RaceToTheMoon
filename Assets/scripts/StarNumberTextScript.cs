afc8304cfaabb96724f964719cb2af758633f3b4
Uzorma <uzorma@live.unc.edu>
2020-03-01 20:23:09 -0500

    Added Text Elements and Scripts
    Added the three text elements and their corresponding scripts

---
+++ Assets/scripts/StarNumberTextScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarNumberTextScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Text>().text = generateStarNoText();
+    }
+
+    // Update is called once per frame
+    public string generateStarNoText()
+    {
+        int stars = PlayerPrefs.GetInt("starsFound");
+        if(stars == 0)
+        {
+            return "And Found 0 Stars";
+        } else if (stars == 1)
+        {
+            return "And Found 1 Star";
+        } else
+        {
+            return "And Found "+ stars.ToString() +" Stars";
+        }
+    }
+}
