f99bf6e296922b2bb545b0b40c579961f37b4fe7
Uzorma <uzorma@live.unc.edu>
2020-04-07 00:08:50 -0400

    UI changes and StarList Script
    Adapted EndScene UI and begun script changes

--- Assets/scripts/StarListScript.cs
+++ Assets/scripts/StarListScript.cs
@@ -7,27 +7,48 @@ using UnityEngine.UI;
 
 public class StarListScript : MonoBehaviour
 {
+    private Text starList;
+    private Vector3 startPosition;
+    private float scrollSpeed = 0.05f;
+    private float scrollPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<Text>().text = generateStarList();
+        starList = this.GetComponent<Text>();
+        startPosition = starList.transform.position;
+
+        Debug.Log(startPosition);
+        Debug.Log(starList.transform.localPosition);
+
+        Debug.Log("Initial Y (position): " + starList.transform.position.y);
+        Debug.Log("Initial Y (localPosition): " + starList.transform.localPosition.y);
     }
 
     // Update is called once per frame
+    void Update(){
+        float height = starList.preferredHeight - 6.0f;
+        scrollPosition = 0.0f + startPosition.y;
+        Debug.Log("Text Height: " + height);
+
+        starList.transform.position = new Vector3(startPosition.x, scrollPosition % height, startPosition.z);
+
+        scrollPosition += scrollSpeed * 20 * Time.deltaTime;
+
+    }
+
+
     public string generateStarList()
     {
         string starsFoundText = "";
         int starsFound = PlayerPrefs.GetInt("starsFound");
+
         for (int x = 0; x < starsFound; x++)
         {
             string getString = "Star_" + (x).ToString();
             starsFoundText += PlayerPrefs.GetString(getString);
             starsFoundText += '\n';
-
-
-            Debug.Log(getString);
-
-          
         }
 
         return starsFoundText;
