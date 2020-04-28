af4f4f09d1386c3a48f02f46069e68d5321da0ea
Mary Gibeau <marygold1011@gmail.com>
2020-04-28 15:13:36 -0400

    Update CursorHiding.cs
    removes level manager from script

--- Assets/scripts/CursorHiding.cs
+++ Assets/scripts/CursorHiding.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class CursorHiding : MonoBehaviour
 {
-    LevelManager lvlr;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
-        lvlr = GameObject.Find("LevelManager").GetComponent<LevelManager>();
     }
 
     // Update is called once per frame
