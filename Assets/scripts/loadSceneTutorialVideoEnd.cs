979ac2600319a0fb645b6f9cb028eb15482af30c
Vulfid <54612746+Vulfid@users.noreply.github.com>
2020-04-27 20:25:22 -0400

    added post tutorial game load cinematic

---
+++ Assets/scripts/loadSceneTutorialVideoEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class loadSceneTutorialVideoEnd : MonoBehaviour
+{
+    public VideoPlayer VideoPlayer; // Drag & Drop the GameObject holding the VideoPlayer component
+
+    void Start()
+    {
+        VideoPlayer.loopPointReached += LoadScene;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+    void LoadScene(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
