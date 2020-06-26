f0d41c3c315f0e652b01e5dc842883abdc722ea2
Mary Gibeau <marygold1011@gmail.com>
2020-06-25 21:26:42 -0400

    Cleans up star list on final score screen

--- Assets/scripts/StarListScript.cs
+++ Assets/scripts/StarListScript.cs
@@ -9,30 +9,43 @@ using TMPro;
 public class StarListScript : MonoBehaviour
 {
     private TextMeshProUGUI starList;
-  
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.GetComponent<TextMeshProUGUI>().text = generateStarList();
         starList = this.GetComponent<TextMeshProUGUI>();
-        
+
     }
 
     public string generateStarList()
     {
         string starsFoundText = "";
         int starsFound = PlayerPrefs.GetInt("starsFound");
-
-        for (int x = 0; x < starsFound; x++)
+        if (starsFound == 1)
         {
-            string getString = "Star_" + (x).ToString();
-            starsFoundText += PlayerPrefs.GetString(getString);
-            starsFoundText += " | "; 
-            
+            for (int i = 3; i > 0; i--)
+            {
+                starsFoundText += "     ";
+                starsFoundText += PlayerPrefs.GetString("Star_0");
+                // starsFoundText += " | ";
+                starsFoundText += "     |";
+            }
         }
+        else
+        {
 
+            for (int x = 0; x < starsFound; x++)
+            {
+                string getString = "Star_" + (x).ToString();
+                starsFoundText += "     ";
+                starsFoundText += PlayerPrefs.GetString(getString);
+                starsFoundText += "     |";
+
+            }
+        }
         return starsFoundText;
     }
-  
+
 }
