7a59e112d0c5d0e13fa63ca1d28cad60ec93530d
Mary Gibeau <marygold1011@gmail.com>
2020-10-18 19:44:14 -0400

    Changes star bar look

--- Assets/scripts/StarBarController.cs
+++ Assets/scripts/StarBarController.cs
@@ -1,28 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StarBarController : MonoBehaviour
 {
-    GameObject[] activeStars;
+    // GameObject[] activeStars;
+    public Texture[] statusBars; 
+    GameObject starBarImage;
     // Start is called before the first frame update
     void Start()
     {
-        activeStars = new GameObject[12];
-        int i = 0;
-        foreach (Transform child in transform)
-        {
-            if (i < 12)
-            {
-                activeStars[i] = child.gameObject;
-                activeStars[i].SetActive(false);
-                i++;
-            }
-        }
+        starBarImage = this.transform.GetChild(0).gameObject;
+        starBarImage.GetComponent<RawImage>().texture = statusBars[0];
     }
 
     public void UpdateStarBar(int starsCollected)
     {
-        activeStars[starsCollected - 1].SetActive(true);
+        starBarImage.GetComponent<RawImage>().texture = statusBars[starsCollected];
     }
 }
